f20c847395cb6c238bfe82847b006bbfe69d3c7b
pzou <ping_zou@intuit.com>
2018-12-17 10:12:54 -0800

    check in comment fix

--- foremast-service/pkg/common/ginutils.go
+++ foremast-service/pkg/common/ginutils.go
@@ -2,6 +2,8 @@ package common
 
 import "github.com/gin-gonic/gin"
 
+
+// ErrorResponse .... use ErrorResponse to handle error
 func ErrorResponse(c *gin.Context, code int, err string) {
 	c.JSON(code, gin.H{
 		"error": err,
