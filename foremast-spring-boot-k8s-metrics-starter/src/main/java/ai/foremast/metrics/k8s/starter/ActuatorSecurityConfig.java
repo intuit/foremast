c2356026c40c67a718eec171088ea1cb9b977eea
xshao <Sheldon_Shao@intuit.com>
2018-12-17 10:08:50 -0800

    Metrics starter for spring boot application

---
+++ foremast-spring-boot-k8s-metrics-starter/src/main/java/ai/foremast/metrics/k8s/starter/ActuatorSecurityConfig.java
@@ -0,0 +1,21 @@
+package ai.foremast.metrics.k8s.starter;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+public class ActuatorSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    /*
+        This spring security configuration does the following
+
+        1. Allow access to the all APIs (/info /health /prometheus).
+     */
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .antMatchers("/actuator/info", "/actuator/health", "/actuator/prometheus", "/metrics")
+                .permitAll();
+    }
+}
