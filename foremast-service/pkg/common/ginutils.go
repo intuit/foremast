dd2c08b57ea651b877f0e748fcc92b276273b8e8
pzou <ping_zou@intuit.com>
2018-12-17 09:44:40 -0800

    check in gofmt files

--- foremast-service/pkg/common/ginutils.go
+++ foremast-service/pkg/common/ginutils.go
@@ -2,7 +2,6 @@ package common
 
 import "github.com/gin-gonic/gin"
 
-
 func ErrorResponse(c *gin.Context, code int, err string) {
 	c.JSON(code, gin.H{
 		"error": err,
--- foremast-service/pkg/models/models.go
+++ foremast-service/pkg/models/models.go
@@ -8,7 +8,7 @@ type MetricQuery struct {
 
 	Parameters map[string]interface{} `json:"parameters,omitempty"`
 
-    // Can be any metric data Source Type
+	// Can be any metric data Source Type
 	// For prometheus dataSource Type
 	//Name string `json:"name,omitempty"`
 	//// For example: error4xx error5xx cpu memory latency etc.
@@ -24,11 +24,9 @@ type MetricQuery struct {
 	//End int64 `json:"end,omitempty"`
 }
 
-
-
 type MetricsInfo struct {
-	Current map[string]MetricQuery `json:"current"`
-	Baseline map[string]MetricQuery `json:"baseline,omitempty"`
+	Current    map[string]MetricQuery `json:"current"`
+	Baseline   map[string]MetricQuery `json:"baseline,omitempty"`
 	Historical map[string]MetricQuery `json:"historical,omitempty"`
 }
 
@@ -51,80 +49,75 @@ type ApplicationHealthAnalyzeRequest struct {
 	Strategy string `json:"strategy"`
 }
 
-
 type AnomalyInfo struct {
-	Tags  string  `json:"tags"`
+	Tags   string  `json:"tags"`
 	Values []int64 `json:"values"`
 }
 
-
-
 type ApplicationHealthAnalyzeResponse struct {
-	JobId string `json:"jobId"`
-	StatusCode int32 `json:"statusCode"`
-	Status string `json:"status"`
-	Reason string `json:"reason,omitempty"`
-	Anomaly map[string]AnomalyInfo  `json:"anomaly",omitempty`
+	JobId      string                 `json:"jobId"`
+	StatusCode int32                  `json:"statusCode"`
+	Status     string                 `json:"status"`
+	Reason     string                 `json:"reason,omitempty"`
+	Anomaly    map[string]AnomalyInfo `json:"anomaly",omitempty`
 }
 
 type ApplicationHealthAnalyzeResponseNew struct {
-	JobId string `json:"jobId"`
-	StatusCode int32 `json:"statusCode"`
-	Status string `json:"status"`
-	Reason string `json:"reason,omitempty"`
-
+	JobId      string `json:"jobId"`
+	StatusCode int32  `json:"statusCode"`
+	Status     string `json:"status"`
+	Reason     string `json:"reason,omitempty"`
 }
 
 type Document struct {
-ID         string    `json:"id"`
-AppName    string    `json:"appName"`
-CreatedAt  time.Time `json:"created_at"`
-StartTime  time.Time `json:"startTime"`
-EndTime    time.Time `json:"endTime"`
-ModifiedAt time.Time `json:"modified_at"`
-CurrentConfig    string    `json:"currentConfig"`
-BaselineConfig    string    `json:"baselineConfig",omitempty`
-HistoricalConfig    string    `json:"historicalConfig",omitempty`
-Status string `json:"status"`
-StatusCode   string `json:"statusCode"`
-Strategy   string   `json:"strategy"`
-Reason       string   `json:"reason",omitempty`
-ProcessingContent string `json:"processingContent",omitempty`
+	ID                string    `json:"id"`
+	AppName           string    `json:"appName"`
+	CreatedAt         time.Time `json:"created_at"`
+	StartTime         time.Time `json:"startTime"`
+	EndTime           time.Time `json:"endTime"`
+	ModifiedAt        time.Time `json:"modified_at"`
+	CurrentConfig     string    `json:"currentConfig"`
+	BaselineConfig    string    `json:"baselineConfig",omitempty`
+	HistoricalConfig  string    `json:"historicalConfig",omitempty`
+	Status            string    `json:"status"`
+	StatusCode        string    `json:"statusCode"`
+	Strategy          string    `json:"strategy"`
+	Reason            string    `json:"reason",omitempty`
+	ProcessingContent string    `json:"processingContent",omitempty`
 }
 
-
 type DocumentRequest struct {
-AppName   string `json:"appName"`
-StartTime string `json:"startTime"`
-EndTime   string `json:"endTime"`
-CurrentConfig   string `json:"contentConfig"`
-BaselineConfig   string `json:"baselineConfig",omitempty`
-HistoricalConfig   string `json:"historicalConfig",omitempty`
-StatusCode   string `json:"statusCode",omitempty`
-Strategy   string   `json:"strategy"`
+	AppName          string `json:"appName"`
+	StartTime        string `json:"startTime"`
+	EndTime          string `json:"endTime"`
+	CurrentConfig    string `json:"contentConfig"`
+	BaselineConfig   string `json:"baselineConfig",omitempty`
+	HistoricalConfig string `json:"historicalConfig",omitempty`
+	StatusCode       string `json:"statusCode",omitempty`
+	Strategy         string `json:"strategy"`
 }
 
 type DocumentResponse struct {
-ID         string    `json:"id"`
-AppName    string    `json:"appName"`
-CreatedAt  time.Time `json:"created_at"`
-StartTime  time.Time `json:"startTime"`
-EndTime    time.Time `json:"endTime"`
-ModifiedAt time.Time `json:"modified_at"`
-//Type       string    `json:"type"`
-Strategy   string   `json:"strategy"`
-CurrentConfig    string    `json:"currentConfig"`
-BaselineConfig    string    `json:"baselineConfig",omitempty`
-HistoricConfig    string    `json:"historicConfig",omitempty`
-Status string `json:"status"`
-StatusCode string `json:"statusCode"`
-Reason  string `json:"reason",omitempty`
-ProcessingContent string `json:"processingContent",omitempty`
-AnomalyInfo string  `json:"anomalyInfo",omitempty`
+	ID         string    `json:"id"`
+	AppName    string    `json:"appName"`
+	CreatedAt  time.Time `json:"created_at"`
+	StartTime  time.Time `json:"startTime"`
+	EndTime    time.Time `json:"endTime"`
+	ModifiedAt time.Time `json:"modified_at"`
+	//Type       string    `json:"type"`
+	Strategy          string `json:"strategy"`
+	CurrentConfig     string `json:"currentConfig"`
+	BaselineConfig    string `json:"baselineConfig",omitempty`
+	HistoricConfig    string `json:"historicConfig",omitempty`
+	Status            string `json:"status"`
+	StatusCode        string `json:"statusCode"`
+	Reason            string `json:"reason",omitempty`
+	ProcessingContent string `json:"processingContent",omitempty`
+	AnomalyInfo       string `json:"anomalyInfo",omitempty`
 }
 
 type SearchResponse struct {
-Time      string             `json:"time"`
-Hits      string             `json:"hits"`
-Documents []DocumentResponse `json:"documents"`
+	Time      string             `json:"time"`
+	Hits      string             `json:"hits"`
+	Documents []DocumentResponse `json:"documents"`
 }
