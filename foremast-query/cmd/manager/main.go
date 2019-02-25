c0c29683ce499813d1e6714b66fe3d0b8e10e124
kjones16 <kian_jones@intuit.com>
2019-02-25 14:16:50 -0800

    add simulation

---
+++ foremast-query/cmd/manager/main.go
@@ -0,0 +1,206 @@
+/*
+Copyright 2018 The Kubernetes Authors.
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+    http://www.apache.org/licenses/LICENSE-2.0
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package main
+
+import (
+	"bufio"
+	"bytes"
+	"encoding/json"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"net/url"
+	"os"
+	"path/filepath"
+	"strings"
+	"time"
+
+	fq "foremast.ai/foremast/foremast-query/pkg/foremastquery"
+	"github.com/gorilla/mux"
+	"k8s.io/apimachinery/pkg/api/errors"
+)
+
+var (
+	masterURL  string
+	kubeconfig string
+)
+
+type JobRequest struct {
+	Query map[string]map[string][]string `json:"query"`
+}
+
+// type JobQuery {
+//   Terms JobTerms
+// }
+//
+// type JobTerms {
+//
+// }
+
+func CheckJobCompleted(jobID string) bool {
+	esUrl := "http://ace26cb17152911e9b3ee067481c81ce-156838986.us-west-2.elb.amazonaws.com:9200/documents/_search"
+	newRequest := JobRequest{
+		Query: map[string]map[string][]string{
+			"terms": {
+				"_id": []string{jobID},
+			},
+		},
+	}
+
+	requestbody, err := json.Marshal(newRequest)
+	if err != nil {
+		log.Printf("req error %s\n", err)
+		return false
+	}
+	log.Printf("reqbody: %#v", string(requestbody))
+
+	completed := false
+	c, err := fq.NewClient(nil, os.Getenv("ENDPOINT"))
+
+	for completed != true {
+		req, err := http.NewRequest("GET", esUrl, bytes.NewReader(requestbody))
+		if err != nil {
+			continue
+		}
+		req.Header.Set("Accept", "application/json")
+		req.Header.Set("Content-Type", "application/json")
+		resp, err := c.HttpClient.Do(req)
+		if err != nil {
+			log.Printf("request err: %#v", err)
+		}
+		body, _ := ioutil.ReadAll(resp.Body)
+		// log.Printf("respbody: %#v\n", string(body))
+		respmap := map[string]interface{}{}
+		_ = json.Unmarshal(body, &respmap)
+		// log.Printf("%#v\n", respmap["hits"])
+		if len(respmap["hits"].(map[string]interface{})["hits"].([]interface{})) < 1 {
+			continue
+		}
+		status := respmap["hits"].(map[string]interface{})["hits"].([]interface{})[0].(map[string]interface{})["_source"].(map[string]interface{})["status"].(string)
+		log.Printf("%s status %#v\n", jobID[0:9], status)
+		if strings.HasPrefix(status, "completed") {
+			completed = true
+		} else {
+			time.Sleep(time.Second * 10)
+		}
+
+	}
+
+	return false
+}
+
+func StartAnalyzing(analyzingRequest fq.ApplicationHealthAnalyzeRequest) (string, error) {
+	//log.Printf("\n\nendpoint: %#v\n\n", analyzingRequest.Metrics.Current["error4xx"].Parameters["endpoint"])
+	// log.Printf("\n\nendpoint: %#v\n\n", os.Getenv("ENDPOINT"))
+	c, err := fq.NewClient(nil, os.Getenv("ENDPOINT")) //analyzingRequest.Metrics.Current["error4xx"].Parameters["endpoint"].(string))
+	b, err := json.Marshal(analyzingRequest)
+	if err != nil {
+		return "", err
+	}
+
+	rel := &url.URL{Path: "create"}
+	u := c.BaseURL.ResolveReference(rel)
+
+	// log.Printf("Request body: %v\n", string(b))
+	req, err := http.NewRequest("POST", u.String(), bytes.NewReader(b))
+	if err != nil {
+
+		return "", err
+	}
+	req.Header.Set("Accept", "application/json")
+	req.Header.Set("User-Agent", c.UserAgent)
+
+	resp, err := c.HttpClient.Do(req)
+	if err != nil {
+		log.Printf("request err: %#v", err)
+		return "", err
+	}
+
+	// log.Printf("response: %#v\n", resp)
+
+	if resp.StatusCode != 200 {
+		return "", errors.NewBadRequest(u.String() + " responded invalid server response:" + resp.Status)
+	}
+	defer resp.Body.Close()
+	var analyzingResponse = fq.ApplicationHealthAnalyzeResponse{}
+
+	//var jobId = ""
+	//body, err := ioutil.ReadAll(resp.Body)
+	err = json.NewDecoder(resp.Body).Decode(&analyzingResponse)
+	if err != nil {
+		return "", err
+	}
+
+	if analyzingResponse.JobId == "" {
+		return "", errors.NewBadRequest(u.String() + " responded invalid server response:" + analyzingResponse.Reason)
+	}
+	return analyzingResponse.JobId, nil
+}
+
+func ForemastQuery(w http.ResponseWriter, r *http.Request) {
+	// decoder := json.NewDecoder(r.Body)
+
+	analyzingRequest := fq.ApplicationHealthAnalyzeRequest{}
+	// err := decoder.Decode(&analyzingRequest)
+	// if err != nil {
+	// 	panic(err)
+	// }
+	// log.Printf("%#v", analyzingRequest)
+	absPath, _ := filepath.Abs("./requestsinput.txt")
+	// absPath, _ := filepath.Abs("siminput/simrequests0.txt")
+	file, err := os.Open(absPath)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		byt := []byte(scanner.Text())
+		if err := json.Unmarshal(byt, &analyzingRequest); err != nil {
+			panic(err)
+		}
+		// log.Printf("%#v\n", analyzingRequest)
+		resp, err := StartAnalyzing(analyzingRequest)
+		if err == nil {
+			log.Printf("startanalyzing resp %#v\n", resp)
+		}
+		CheckJobCompleted(resp)
+	}
+
+}
+
+func serve() {
+	router := mux.NewRouter()
+
+	// router.HandleFunc("/restart", ForemastQuery).Methods("GET")
+
+	log.Printf("Service started on port:8011, mode:\n")
+	log.Fatal(http.ListenAndServe(":8011", router))
+
+}
+
+func main() {
+	//serve()
+	// for true {
+	ForemastQuery(nil, nil)
+	// time.Sleep(time.Second * 60)
+	// }
+
+}
+
+//func init() {
+//	flag.StringVar(&kubeconfig, "kubeconfig", "", "Path to a kubeconfig. Only required if out-of-cluster.")
+//	flag.StringVar(&masterURL, "master", "", "The address of the Kubernetes API server. Overrides any value in kubeconfig. Only required if out-of-cluster.")
+//}
---
+++ foremast-query/cmd/manager/wf.py
@@ -0,0 +1,50 @@
+import json, requests, random
+
+basetext = '{"appName":"APP_CHANGE","startTime":"2018-11-03T16:50:04-07:00","endTime":"2018-11-03T16:33:04-07:00", "metrics":{"current":{"error4xx":{"dataSourceType":"wavefront","parameters":{"end":ENDINGTIME,"endpoint":"http://ab683be21d97f11e88e87023426427de-657499332.us-west-2.elb.amazonaws.com:9090/api/v1/","query":"sum(ts(telegraf.http.server.requests.count, env=\\\"prd\\\" and app=\\\"REPLACE\\\" and status=\\\"5*\\\"))","start":STARTINGTIME,"step":60}},"latency":{"dataSourceType":"wavefront","parameters":{"end":ENDINGTIME,"endpoint":"http://ab683be21d97f11e88e87023426427de-657499332.us-west-2.elb.amazonaws.com:9090/api/v1/","query":"avg(ts(telegraf.http.server.requests.mean, env=\\\"prd\\\" and status=\\\"2*\\\" and app=\\\"REPLACE\\\"), app)","start":STARTINGTIME,"step":60}}},"historical":{"error4xx":{"dataSourceType":"wavefront","parameters":{"end":ENDINGTIME,"endpoint":"http://ab683be21d97f11e88e87023426427de-657499332.us-west-2.elb.amazonaws.com:9090/api/v1/","query":"sum(ts(telegraf.http.server.requests.count, env=\\\"prd\\\" and app=\\\"REPLACE\\\" and status=\\\"5*\\\"))","start":STARTINGTIME,"step":60}},"latency":{"dataSourceType":"wavefront","parameters":{"end":ENDINGTIME,"endpoint":"http://ab683be21d97f11e88e87023426427de-657499332.us-west-2.elb.amazonaws.com:9090/api/v1/","query":"avg(ts(telegraf.http.server.requests.mean, env=\\\"prd\\\" and status=\\\"2*\\\" and app=\\\"REPLACE\\\"), app)","start":STARTINGTIME,"step":60}}}},"strategy":"rollover"}\n'
+
+url = 'https://intuit.wavefront.com/api/v2/chart/metric/detail?m=appdynamics.apm.transactions.50th_percentile_resp_time_ms'
+apitoken = {'Authorization': 'Bearer ba2e4e71-cf26-4e7c-afce-55b58446f991'}
+
+r = requests.get(url,headers=apitoken)
+
+type = "json" # "json"
+# print(r.json()["hosts"])
+# print(r.json()["hosts"])
+
+i = 0
+count = 0
+# for host in r.json()["hosts"]:
+    # if i > 100:
+    #     count += 1
+    #     i = 0
+    # filename = ""
+    # if type == "json":
+    #     filename = "siminput/simrequests" + str(count) + ".txt"
+    # else:
+    #     filename = "siminput/simrequests" + str(count) + ".csv"
+    # with open(filename, "a+") as f:
+    #     starttime = random.randint(1550052000000, 1550055600000) #10 to 11 am
+    #     endtime = random.randint(1550073600000, 1550077200000) #4 to 5 pm
+    #     if type == "json":
+    #         curtext = basetext
+    #         curtext = curtext.replace('STARTINGTIME', str(starttime))
+    #         curtext = curtext.replace('ENDINGTIME', str(endtime))
+    #         curtext = curtext.replace('APP_CHANGE', host["host"])
+    #         curtext = curtext.replace('REPLACE', host["tags"]["app"])
+    #
+    #         f.write(curtext)
+    #
+    #     else:
+    #         line = ""
+    #         line += host["tags"]["app"] + ","
+    #         line += "4xx" + "," + "sum(ts(telegraf.http.server.requests.count, env=\\\"prd\\\" and app=\\\"REPLACE\\\" and status=\\\"4*\\\"))".replace("REPLACE", host["tags"]["app"]) + ","
+    #         line += "5xx" + "," + "sum(ts(telegraf.http.server.requests.count, env=\\\"prd\\\" and app=\\\"REPLACE\\\" and status=\\\"5*\\\"))".replace("REPLACE", host["tags"]["app"]) + "\n"
+    #         f.write(line)
+    # i += 1
+
+with open("appnames.txt", "a+") as f:
+    apps = {}
+    for host in r.json()["hosts"]:
+        apps[host["host"]] = True
+    for host in apps.keys():
+        f.write(host+"\n")
