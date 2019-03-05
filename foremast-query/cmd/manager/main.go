edd067fd2f0e8bf73c6437d7dd36797ea5ea8786
kjones16 <kian_jones@intuit.com>
2019-03-04 17:35:24 -0800

    updated

--- foremast-query/cmd/manager/main.go
+++ foremast-query/cmd/manager/main.go
@@ -17,6 +17,7 @@ import (
 	"bufio"
 	"bytes"
 	"encoding/json"
+	"fmt"
 	"io/ioutil"
 	"log"
 	"net/http"
@@ -40,16 +41,18 @@ type JobRequest struct {
 	Query map[string]map[string][]string `json:"query"`
 }
 
-// type JobQuery {
-//   Terms JobTerms
-// }
-//
-// type JobTerms {
-//
-// }
+type JobInfo struct {
+	JobID        string
+	ErrorQuery   string
+	LatencyQuery string
+	Request      fq.ApplicationHealthAnalyzeRequest
+}
+
+var jobmap map[string]JobInfo
 
 func CheckJobCompleted(jobID string) bool {
-	esUrl := "http://ace26cb17152911e9b3ee067481c81ce-156838986.us-west-2.elb.amazonaws.com:9200/documents/_search"
+	// esUrl := "http://ace26cb17152911e9b3ee067481c81ce-156838986.us-west-2.elb.amazonaws.com:9200/documents/_search"
+	esUrl := "http://a2c1d2f06186b11e98f4602f39e94cef-36929393.us-west-2.elb.amazonaws.com:9200/documents/_search"
 	newRequest := JobRequest{
 		Query: map[string]map[string][]string{
 			"terms": {
@@ -91,6 +94,7 @@ func CheckJobCompleted(jobID string) bool {
 		log.Printf("%s status %#v\n", jobID[0:9], status)
 		if strings.HasPrefix(status, "completed") {
 			completed = true
+			return true
 		} else {
 			time.Sleep(time.Second * 10)
 		}
@@ -100,10 +104,16 @@ func CheckJobCompleted(jobID string) bool {
 	return false
 }
 
+func CheckAllJobsCompleted() {
+
+}
+
 func StartAnalyzing(analyzingRequest fq.ApplicationHealthAnalyzeRequest) (string, error) {
 	//log.Printf("\n\nendpoint: %#v\n\n", analyzingRequest.Metrics.Current["error4xx"].Parameters["endpoint"])
-	log.Printf("\nendpoint: %#v\n", os.Getenv("ENDPOINT"))
-	c, err := fq.NewClient(nil, os.Getenv("ENDPOINT")) //analyzingRequest.Metrics.Current["error4xx"].Parameters["endpoint"].(string))
+	endpoint := "http://" + os.Getenv("FOREMAST_SERVICE_SERVICE_HOST") + ":" + os.Getenv("FOREMAST_SERVICE_SERVICE_PORT_HTTP") + "/v1/healthcheck/create" //os.Getenv("ENDPOINT")
+
+	log.Printf("\nendpoint: %#v\n", endpoint)
+	c, err := fq.NewClient(nil, endpoint) //analyzingRequest.Metrics.Current["error4xx"].Parameters["endpoint"].(string))
 	b, err := json.Marshal(analyzingRequest)
 	if err != nil {
 		return "", err
@@ -113,7 +123,7 @@ func StartAnalyzing(analyzingRequest fq.ApplicationHealthAnalyzeRequest) (string
 	u := c.BaseURL.ResolveReference(rel)
 
 	// log.Printf("Request body: %v\n", string(b))
-	req, err := http.NewRequest("POST", os.Getenv("ENDPOINT"), bytes.NewReader(b))
+	req, err := http.NewRequest("POST", endpoint, bytes.NewReader(b))
 	if err != nil {
 
 		return "", err
@@ -148,36 +158,64 @@ func StartAnalyzing(analyzingRequest fq.ApplicationHealthAnalyzeRequest) (string
 	return analyzingResponse.JobId, nil
 }
 
-func ForemastQuery(w http.ResponseWriter, r *http.Request) {
-	// decoder := json.NewDecoder(r.Body)
+func ForemastQuery(appName string, errorQuery string, latencyQuery string) bool {
+	now := time.Now()
+	nanos := now.UnixNano()
+	millis := nanos / 1000000
+	startTime := millis - (60 * 5)
+	endTime := startTime + (60 * 30)
+
+	sample := `{"appName":"fds-dac","startTime":"2018-11-03T16:50:04-07:00","endTime":"2018-11-03T16:33:04-07:00","strategy":"rollover","metrics":{"current":{"error4xx":{"dataSourceType":"wavefront","parameters":{"end":1550825400000,"endpoint":"http://ab683be21d97f11e88e87023426427de-657499332.us-west-2.elb.amazonaws.com:9090/api/v1/","query":"","start":1550815200000,"step":60}},"latency":{"dataSourceType":"wavefront","parameters":{"end":1550825400000,"endpoint":"http://ab683be21d97f11e88e87023426427de-657499332.us-west-2.elb.amazonaws.com:9090/api/v1/","query":"avg(align(60s, mean, ts(appdynamics.apm.transactions.avg_resp_time_ms, bu=cto and app=fds-dac and env=prd and source=fds-dac )), name)","start":1550815200000,"step":60}}},"historical":{"error4xx":{"dataSourceType":"wavefront","parameters":{"end":1550825400000,"endpoint":"http://ab683be21d97f11e88e87023426427de-657499332.us-west-2.elb.amazonaws.com:9090/api/v1/","query":"avg(align(60s, mean, ts(appdynamics.apm.transactions.errors_per_min, bu=cto and app=fds-dac and env=prd and source=fds-dac )), name)","start":1550815200000,"step":60}},"latency":{"dataSourceType":"wavefront","parameters":{"end":1550825400000,"endpoint":"http://ab683be21d97f11e88e87023426427de-657499332.us-west-2.elb.amazonaws.com:9090/api/v1/","query":"avg(align(60s, mean, ts(appdynamics.apm.transactions.avg_resp_time_ms, bu=cto and app=fds-dac and env=prd and source=fds-dac )), name)","start":1550815200000,"step":60}}},"strategy":"rollover"}}`
 
 	analyzingRequest := fq.ApplicationHealthAnalyzeRequest{}
-	// err := decoder.Decode(&analyzingRequest)
-	// if err != nil {
-	// 	panic(err)
-	// }
-	// log.Printf("%#v", analyzingRequest)
-	absPath, _ := filepath.Abs("./requestsinput.txt")
-	// absPath, _ := filepath.Abs("siminput/simrequests0.txt")
-	file, err := os.Open(absPath)
-	if err != nil {
-		log.Fatal(err)
+	byt := []byte(sample)
+	if err := json.Unmarshal(byt, &analyzingRequest); err != nil {
+		panic(err)
 	}
-	defer file.Close()
 
-	scanner := bufio.NewScanner(file)
-	for scanner.Scan() {
-		byt := []byte(scanner.Text())
-		if err := json.Unmarshal(byt, &analyzingRequest); err != nil {
-			panic(err)
-		}
-		// log.Printf("%#v\n", analyzingRequest)
-		resp, err := StartAnalyzing(analyzingRequest)
-		if err == nil {
-			log.Printf("startanalyzing resp %#v\n", resp)
-		}
-		CheckJobCompleted(resp)
+	endpoint := "http://" + os.Getenv("FOREMAST_SERVICE_SERVICE_HOST") + ":" + os.Getenv("FOREMAST_SERVICE_SERVICE_PORT_HTTP") + "/v1/healthcheck/create" //os.Getenv("ENDPOINT")
+
+	analyzingRequest.AppName = appName
+	fmt.Printf("%s, %s", analyzingRequest.AppName, appName)
+	analyzingRequest.Metrics.Current["error4xx"].Parameters["query"] = errorQuery
+	analyzingRequest.Metrics.Current["error4xx"].Parameters["endpoint"] = endpoint
+	analyzingRequest.Metrics.Current["error4xx"].Parameters["start"] = startTime
+	analyzingRequest.Metrics.Current["error4xx"].Parameters["end"] = endTime
+	analyzingRequest.Metrics.Current["latency"].Parameters["query"] = latencyQuery
+	analyzingRequest.Metrics.Current["latency"].Parameters["endpoint"] = endpoint
+	analyzingRequest.Metrics.Current["latency"].Parameters["start"] = startTime
+	analyzingRequest.Metrics.Current["latency"].Parameters["end"] = endTime
+	analyzingRequest.Metrics.Historical["error4xx"].Parameters["query"] = errorQuery
+	analyzingRequest.Metrics.Historical["error4xx"].Parameters["endpoint"] = endpoint
+	analyzingRequest.Metrics.Historical["error4xx"].Parameters["start"] = startTime
+	analyzingRequest.Metrics.Historical["error4xx"].Parameters["end"] = endTime
+	analyzingRequest.Metrics.Historical["latency"].Parameters["query"] = latencyQuery
+	analyzingRequest.Metrics.Historical["latency"].Parameters["endpoint"] = endpoint
+	analyzingRequest.Metrics.Historical["latency"].Parameters["start"] = startTime
+	analyzingRequest.Metrics.Historical["latency"].Parameters["end"] = endTime
+	b, err := json.MarshalIndent(analyzingRequest, "", "  ")
+	if err != nil {
+		fmt.Println("error:", err)
 	}
+	_ = b
+	// fmt.Print(string(b))
+
+	resp, err := StartAnalyzing(analyzingRequest)
+	if err == nil {
+		// log.Printf("startanalyzing resp %#v\n", resp)
+		jobmap[analyzingRequest.AppName] = JobInfo{
+			ErrorQuery:   analyzingRequest.Metrics.Current["error4xx"].Parameters["query"].(string),
+			LatencyQuery: analyzingRequest.Metrics.Current["latency"].Parameters["query"].(string),
+			JobID:        resp,
+			Request:      analyzingRequest,
+		}
+		log.Printf("jobmap %#v", jobmap)
+		return true
+	} else {
+		log.Printf("startanalyzing err %#v\n%#v\n", err, err.Error())
+		return false
+	}
+	// CheckJobCompleted(resp)
 
 }
 
@@ -192,11 +230,37 @@ func serve() {
 }
 
 func main() {
-	//serve()
-	// for true {
-	ForemastQuery(nil, nil)
-	// time.Sleep(time.Second * 60)
-	// }
+	jobmap = make(map[string]JobInfo)
+
+	// decoder := json.NewDecoder(r.Body)
+
+	// UpdateTimes(analyzingRequest)
+	absPath, _ := filepath.Abs("./requests.csv")
+	file, err := os.Open(absPath)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		line := scanner.Text()
+		values := strings.Split(line, ";")
+		success := ForemastQuery(values[0], values[2], values[4])
+		for success != true {
+			time.Sleep(time.Second * 3)
+			ForemastQuery(values[0], values[2], values[4])
+		}
+		// time.Sleep(time.Second * 60)
+		// }
+	}
+
+	for {
+		if CheckJobCompleted(jobmap["fds-fpp"].JobID) {
+			// UpdateTimes(&jobmap[k].Request)
+			ForemastQuery("fds-fpp", jobmap["fds-fpp"].ErrorQuery, jobmap["fds-fpp"].LatencyQuery)
+		}
+	}
 
 }
 
