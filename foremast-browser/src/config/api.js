dd604d04cfa4bfde0aab296ce51729d9717dadfc
David Masselink <davemasselink@gmail.com>
2019-03-06 09:50:24 -0800

    #84 - further clean-up this bugfix, moving array copying into ScatterChart component directly (#96)

--- foremast-browser/src/config/api.js
+++ foremast-browser/src/config/api.js
@@ -1,11 +1,6 @@
-//domain is either a specific URI for localhost requests, or an empty string
-//such that nginx will proxy API requests in other deploy scenarios
-export const dataDomain =
-  (/localhost/.test(window.location.href)) ?
-    'http://foremast-service:8099' : '';
-export const dataPath = '/api/v1/query_range';
-export const dataQueryParam = 'query';
-export const dataStartParam = 'start';
-export const dataEndParam = 'end';
-export const dataStepParam = 'step';
-export const dataStepValSec = 15; //data granularity
+export const DATA_PATH = '/api/v1/query_range';
+export const DATA_QUERY_PARAM = 'query';
+export const DATA_START_PARAM = 'start';
+export const DATA_END_PARAM = 'end';
+export const DATA_STEP_PARAM = 'step';
+export const DATA_STEP_VAL_SEC = 15; //data granularity
