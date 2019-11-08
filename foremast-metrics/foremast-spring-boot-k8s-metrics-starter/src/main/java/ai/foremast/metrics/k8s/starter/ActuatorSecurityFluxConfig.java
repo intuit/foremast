5ae42288831234830e21e7bd3e28e395b8960c7c
Mike Kogan <mike_kogan@intuit.com>
2019-11-08 12:28:03 -0800

    Conditional WebFlux or MVC support in starter done during Inuit G.E.D

--- foremast-metrics/foremast-spring-boot-k8s-metrics-starter/src/main/java/ai/foremast/metrics/k8s/starter/ActuatorSecurityFluxConfig.java
+++ foremast-metrics/foremast-spring-boot-k8s-metrics-starter/src/main/java/ai/foremast/metrics/k8s/starter/ActuatorSecurityFluxConfig.java
@@ -4,18 +4,20 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnWebApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.annotation.Order;
 import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.web.server.SecurityWebFilterChain;
+import org.springframework.security.web.server.util.matcher.ServerWebExchangeMatchers;
 
 @Configuration
 @Order(101)
 @EnableConfigurationProperties({K8sMetricsProperties.class})
 @ConditionalOnClass(AuthenticationManager.class)
-@ConditionalOnWebApplication(type = ConditionalOnWebApplication.Type.SERVLET)
-public class ActuatorSecurityConfig extends WebSecurityConfigurerAdapter {
+@ConditionalOnWebApplication(type = ConditionalOnWebApplication.Type.REACTIVE)
+public class ActuatorSecurityFluxConfig {
 
 
     @Autowired
@@ -27,13 +29,16 @@ public class ActuatorSecurityConfig extends WebSecurityConfigurerAdapter {
         1. Allow access to specific APIs (/info /health /prometheus).
         2. Allow access to "/actuator/k8s-metrics/*"
      */
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
+
+    @Bean
+    public SecurityWebFilterChain apiHttpSecurity(
+            ServerHttpSecurity http) {
         if (k8sMetricsProperties.isDisableCsrf()) {
             http.csrf().disable();
         }
-        http.authorizeRequests()
-                .antMatchers("/actuator/info", "/actuator/health", "/actuator/prometheus", "/metrics", "/actuator/k8s-metrics/*")
-                .permitAll();
+        http.securityMatcher(ServerWebExchangeMatchers.pathMatchers("/actuator/info", "/actuator/health", "/actuator/prometheus", "/metrics", "/actuator/k8s-metrics/*"))
+                .authorizeExchange().anyExchange().permitAll();
+        return http.build();
     }
+
 }
